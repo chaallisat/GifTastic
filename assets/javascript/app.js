dedab0592d1dcd5ca5392f4d63597a49df418e09
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-06-23 16:43:10 -0700

    finished js and html update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,34 +2,64 @@
 const topics = ["Rugrats", "CatDog", "Power Puff Girls", "The Flintstones", "Kim Possible", "Scooby-Doo", "Looney Tunes", "ThunderCats", "DuckTales"];
 
 //displayGifs function to display content to HTML
-// const gifs = $(this).attr("datainame");
-// const queryURL = "..." + "...";
+function displayGifs() {
 
-"https://api.giphy.com/v1/gifs/search?api_key=8iYztG9g8pKSpLVmvZma6qEoSc9e2dxw&q=" + topics + "&limit=10&offset=0&rating=G&lang=en"
+    const gifs = $(this).attr("data-name");
+    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8iYztG9g8pKSpLVmvZma6qEoSc9e2dxw&q=" + gifs + "&limit=10&lang=en"
+    
+    //create an AJAX call for the specific topic button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        
+        //Create a div to hold the gifs
+        const gifDiv = $("<div class='gif'>");
+        
+        //Storing the rating
+        const rating = response.Rated;
+        
+        //Creating an element to display rating
+        const ratingDiv = $("<p>").text("Rating: " + rating);
+        
+        //Display the rating
+        //under every gif, display its rating
+        gifDiv.append(ratingDiv);
+        $("#view-gifs").prepend(gifDiv);
+    });
+            
+}
 
-//create an AJAX call for the specific topic button being clicked
-// $.ajax({
-    // url: queryURL,
-    //method: "GET"
-// }).then(function(response){
 
-// })
-
-//create buttons from the topics | a loop might help
+//Function to display the gifs
+function renderButtons() {
+    $("#button-view").empty();
+    
+    //create buttons from the topics | a loop might help
+    for (let i = 0; i < topics.length; i++) {
+        const bb = $("<button>");
+        bb.addClass("gif-btn")
+        bb.attr("data-name", topics[i])
+        bb.text(topics[i]);
+        $("#button-view").append(bb);
+    }
+}
 
 //on button click, 10 static, non-animated gifs should append to page
-// $("#div").on("click", function(event){
-    // event.preventDefault();
+$("#add-topic").on("click", function(event){
+    event.preventDefault();
 
     //This should grab the new topic from user
-    // const userTopic = $("#another-div").val().trim();
+    const userTopic = $("#topic-input").val().trim();
 
     //Add the topic to our array
-    // topics.push(userTopic);
-// })
+    topics.push(userTopic);
+    renderButtons();
+})
+
+$(document).on("clcik", ".gif-btn", displayGifs);
+renderButtons();
 
 //on gif click, gif should animate
 //on gif click, gif should stop
-//under every gif, display its rating
-//add a form to page that takes the value and store it in topics array and makes a new button on page
 //
